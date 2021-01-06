6835fe25d14183281a55fd0f29fbc1b9a93cf94c
Mexiker <costin.botescu@gmail.com>
2021-01-06 17:24:07 +0200

    win close,app quit

--- dico/main.py
+++ dico/main.py
@@ -5,9 +5,14 @@ import gi
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk,GLib
 
+def quit(widget, mainloop):
+	mainloop.quit()
+	return True
+
 def main():
 	mainloop = GLib.MainLoop()
 	win = Gtk.Window()
+	win.connect('close-request', quit, mainloop)
 	win.show()
 	mainloop.run()
 
