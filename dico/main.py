a4117b2ebc68ec4ca670e50b7005f22f940c8475
Mexiker <costin.botescu@gmail.com>
2021-01-15 17:05:10 +0200

    save dim x y

--- dico/main.py
+++ dico/main.py
@@ -5,13 +5,17 @@ import gi
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk,GLib
 
+import base
+
 def quit(widget, mainloop):
+	base.write(widget)
 	mainloop.quit()
 	return True
 
 def main():
 	mainloop = GLib.MainLoop()
 	win = Gtk.Window()
+	base.read(win)
 	win.connect('close-request', quit, mainloop)
 	win.show()
 	mainloop.run()
