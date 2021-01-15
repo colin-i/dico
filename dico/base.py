bb64f5b166c062034918c0527ab0d4a7d17c5d92
Mexiker <costin.botescu@gmail.com>
2021-01-15 17:06:50 +0200

    rest of mods, prev commit

---
+++ dico/base.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+def get_root_file():
+	return os.path.join(os.path.dirname(os.path.realpath(__file__)),'config.json')
+
+def write(win):
+	d={}
+	dim=win.get_default_size()
+	d['width']=dim.width
+	d['height']=dim.height
+	with open(get_root_file(), "w") as write_file:
+		json.dump(d, write_file)
+
+def read(win):
+	try:
+		with open(get_root_file()) as f:
+			d=json.load(f)
+			win.set_default_size(d['width'],d['height'])
+	except Exception:
+		pass
