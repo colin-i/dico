4214caab1c889869157240a1dc1804dac3f94c51
maxtar <costin.botescu@gmail.com>
2021-01-22 17:54:31 +0200

    sets on main

--- dico/sets.py
+++ dico/sets.py
@@ -3,6 +3,12 @@ import gi
 from gi.repository import Gtk
 
 def ini(b,win):
+	#flg=Gtk.DialogFlags.DESTROY_WITH_PARENT | Gtk.DialogFlags.MODAL
 	d=Gtk.Dialog(title="Settings",transient_for=win)
-	#,flags=Gtk.DialogFlags.DESTROY_WITH_PARENT | Gtk.DialogFlags.MODAL)
+	d.set_modal(True)
+	if win.is_maximized():
+		d.maximize()
+	else:
+		dim=win.get_default_size()
+		d.set_default_size(dim.width,dim.height)
 	d.show()
