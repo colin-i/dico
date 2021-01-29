8626578320cb427b7d6eecff35dd84e49ae14da3
maxtar <costin.botescu@gmail.com>
2021-01-29 17:42:59 +0200

    book

--- dico/layout.py
+++ dico/layout.py
@@ -5,6 +5,12 @@ from gi.repository import Gtk
 import sets
 
 def show(w):
+	bx=Gtk.Box()
+	bx.set_orientation(Gtk.Orientation.VERTICAL)
 	b=Gtk.Button.new_with_label(chr(0x2699))
 	b.connect('clicked', sets.ini, w)
-	w.set_child(b)
+	bx.append(b)
+	pags=Gtk.Notebook()
+	pags.append_page(Gtk.TreeView(),Gtk.Label(label="HubList"))
+	bx.append(pags)
+	w.set_child(bx)
