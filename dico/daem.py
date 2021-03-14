e6994befca9f2fccfce5df362ce596ee5acad260
maxtar <costin.botescu@gmail.com>
2021-03-14 18:14:58 +0200

    keep alive

--- dico/daem.py
+++ dico/daem.py
@@ -9,14 +9,17 @@ from . import hubscon
 from . import main
 
 def confs():
+	global keep
+	keep=data.get_text()
 	return sets.entry("Daemon parameters",data)
 def store(d):
 	d['daemon_args']=data.get_text()
 def restore(d):
 	data.set_text(d['daemon_args'],-1)
 def reset():
-	main.dclose()
-	restart()
+	if keep!=data.get_text():
+		main.dclose()
+		restart()
 
 def restart():
 	main.dopen()
