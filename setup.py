0e8d1999ebf073fa01417a33cd223a3fea09706e
bot <costin.botescu@gmail.com>
2021-03-30 20:43:08 +0300

    more info

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+#setuptools.setup is looking at one argv parameter; to "build" and "install":
+#python3 setup.py install
+
 pkname='dicopp'
 
 import pathlib
@@ -7,7 +10,7 @@ README = (HERE / "README.md").read_text()
 from setuptools import setup
 setup(name=pkname,
 	install_requires=["PyGObject>=3.40","requests>=2.21"],
-	version='1.0.1',
+	version='1.0.2',
 	description='Direct Connect ++ client',
 	long_description=README,
 	long_description_content_type="text/markdown",
