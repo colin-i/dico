f2b20c3f3697487c298af1e61becee7b6f27aba0
bc <costin.botescu@gmail.com>
2023-07-19 09:06:28 +0300

    cleanup

--- dicopp/main.py
+++ dicopp/main.py
@@ -2,6 +2,9 @@ import gi
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk,GLib
 
+import os
+import sys
+
 from . import base
 from . import layout 
 from . import limit
@@ -28,6 +31,9 @@ def quit(widget, mainloop):
 	return True
 
 def main():
+	if len(sys.argv)>1:
+		cleanup()
+		return
 	first.ini()
 	mainloop = GLib.MainLoop()
 	win = Gtk.Window()
@@ -48,6 +54,37 @@ def main():
 	base.read2(d)#after daemon start
 	win.show()
 	mainloop.run()
+
+def cleanup():
+	#remove config and exit
+	c=base.get_client_dir()
+	if os.path.isdir(c):
+		print("Would remove:");
+		f=base.get_client()
+		if os.path.isfile(f):
+			print(f)
+		else:
+			f=None
+		print(c)
+		print("yes ?");
+		str = ""
+		while True:
+			x = sys.stdin.read(1) # reads one byte at a time, similar to getchar()
+			if x == '\n':
+				break
+			str += x
+		if str=="yes":
+			r=" removed"
+			if f:
+				os.remove(f)
+				print(f+r)
+			if len(os.listdir(path=c))==0:
+				os.rmdir(c) #OSError if not empty
+				print(c.__str__()+r)
+			else:
+				print(c.__str__()+" is not empty.")
+		else:
+			print("expecting \"yes\"")
 
 if __name__ == "__main__":
     main()
